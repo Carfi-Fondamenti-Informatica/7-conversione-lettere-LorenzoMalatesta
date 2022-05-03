b5e6fe89fb521625aa94ee25c8cc8cdc6ef9afe6
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-05-03 21:09:49 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool carattere (char &a) {
+    if ((a >= 'a') && (a <= 'z')) {
+        a = a - ' ';
+        return true;
+    } else if ((a >= 'A') && (a <= 'Z')){
+        a = a + ' ';
+        return true;
+    }
+    else {
+        return false;
+    }
+}
